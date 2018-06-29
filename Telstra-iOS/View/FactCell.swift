276d875c139dcffcff10ac5fb307b092b6933613
Hitesh Vaghasiya <hitu.vaghasiya@gmail.com>
2018-06-29 15:44:43 +0530

    - created cell and layout component using VFL - filled table view with fact data

---
+++ Telstra-iOS/View/FactCell.swift
@@ -0,0 +1,24 @@
+//
+//  FactCell.swift
+//  Telstra-iOS
+//
+//  Created by Arun on 6/29/18.
+//  Copyright © 2018 Hitesh. All rights reserved.
+//
+
+import UIKit
+
+class FactCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
